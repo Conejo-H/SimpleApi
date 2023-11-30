dbb1dd8cd998704b897976a7358b4f7c39266ab7
Conejo-H <152205517+Conejo-H@users.noreply.github.com>
2023-11-30 17:04:49 -0600

    Basic project uploaded

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('start'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Rutas 
+//Endpoint de GetUsers
+app.get("/", (req, res) => {
+	res.json("The index is working fine");
+});
+
+//Nos conectamos al puerto 8000
+app.listen(port, ()=> console.log(`Escuchando al puerto ${port}`));
