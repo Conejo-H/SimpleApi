31082eb9e1ae33f0369b1ed6223464ebc707ed0c
Alfonso <hconejo@easysoft.com.mx>
2023-12-05 11:08:05 -0600

    Updated user validation

--- middleware/basicAuth.js
+++ middleware/basicAuth.js
@@ -6,9 +6,9 @@ const mwBasicAuth = async (req , res, next) => {
 
 	const user = await auth(req);
 	console.log(user.name, user.pass);
+
 	//Validamos el usuario
-	if (user.name !== '' || user.pass !== ''){
-		
+	if (user.name !== '' || user.pass !== '' || user.name !== undefined || user.pass !== undefined ){
 		db.obtenerCredencialesUsuario(user.name, user.pass).then(item =>{
 			// console.log(`Este valor regresa la consulta de inicio de sesión:  ${item[0].encontrado}`);
 			if (item[0].encontrado === 1 ){
