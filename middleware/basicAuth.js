8d960709696c574ea371f1e599b007e0afa6ba36
Alfonso <hconejo@easysoft.com.mx>
2023-12-05 11:20:07 -0600

    Added try catch to auth

--- middleware/basicAuth.js
+++ middleware/basicAuth.js
@@ -4,26 +4,30 @@ const db = require('../database/mysql');
 //Creamos nuestra función para usar Basic Authentication
 const mwBasicAuth = async (req , res, next) => {
 
-	const user = await auth(req);
-	console.log(user.name, user.pass);
+	
+	try{
+		const user = await auth(req);
+		console.log(user.name, user.pass);
 
-	//Validamos el usuario
+			//Validamos el usuario
 	if (user.name !== '' || user.pass !== '' || user.name !== undefined || user.pass !== undefined ){
 		db.obtenerCredencialesUsuario(user.name, user.pass).then(item =>{
-			// console.log(`Este valor regresa la consulta de inicio de sesión:  ${item[0].encontrado}`);
-			if (item[0].encontrado === 1 ){
-				next();
-			}
-			else{
-				console.log('Basic Auth: failed')
-				res.statusCode = 401
-				res.end('Acceso no autorizado')
-			}			
-		});
+		   // console.log(`Este valor regresa la consulta de inicio de sesión:  ${item[0].encontrado}`);
+		   if (item[0].encontrado === 1 ){
+			   next();
+		   }
+		   else{
+			   console.log('Basic Auth: failed')
+			   res.statusCode = 401
+			   res.end('Acceso no autorizado')
+		   }			
+	   });
+   }
 	}
-	else{
-		res.end('Acceso no autorizado')
+	catch(error){
+		res.send('Acceso no autorizado');
 	}
+
 }
 
 module.exports = mwBasicAuth;
