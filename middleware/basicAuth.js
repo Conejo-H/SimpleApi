44092dc28db0eaccfce723da92d3f5cc15033eb1
Alfonso <hconejo@easysoft.com.mx>
2023-12-05 10:04:16 -0600

    Validate if credentials are null

--- middleware/basicAuth.js
+++ middleware/basicAuth.js
@@ -8,17 +8,23 @@ const mwBasicAuth = async (req , res, next) => {
 	console.log(user);
 
 	//Validamos el usuario
-	db.obtenerCredencialesUsuario(user.name, user.pass).then(item =>{
-		// console.log(`Este valor regresa la consulta de inicio de sesión:  ${item[0].encontrado}`);
-		if (item[0].encontrado === 1 ){
-			next();
-		}
-		else{
-			console.log('Basic Auth: failed')
-			res.statusCode = 401
-			res.send('Access denied')
-		}			
-	});
+	if (user.name != undefined && user.pass != undefined){
+		console.log(user.name, user.pass);
+		db.obtenerCredencialesUsuario(user.name, user.pass).then(item =>{
+			// console.log(`Este valor regresa la consulta de inicio de sesión:  ${item[0].encontrado}`);
+			if (item[0].encontrado === 1 ){
+				next();
+			}
+			else{
+				console.log('Basic Auth: failed')
+				res.statusCode = 401
+				res.send('Access denied')
+			}			
+		});
+	}
+	else{
+		res.send('Access denied')
+	}
 }
 
 module.exports = mwBasicAuth;
